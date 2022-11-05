c2a0aca07ef43052ff4a2f012aec705741c657a0
wang3ng <56563071+wang3ng@users.noreply.github.com>
2022-11-05 13:58:06 -0400

    Basic Structure

---
+++ PlutoGGJ/Assets/Script/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Vector3 StartPostion;
+    public float PlayerSpeed;
+    public float MoveX;
+    private Rigidbody2D rd;
+    // Use this for initialization
+    void Start()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, StartPostion, 10000);
+        rd = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 playerposition = transform.position;
+        //float MoveX = 0;
+        //float MoveY = 0;
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    MoveY += Time.deltaTime * PlayerSpeed;
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    MoveY -= Time.deltaTime * PlayerSpeed;
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    MoveX -= Time.deltaTime * PlayerSpeed;
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    MoveX += Time.deltaTime * PlayerSpeed;
+        //}
+        //if (playerposition.x + MoveX <= -8 || playerposition.x + MoveX >= 8.5) MoveX = 0;
+        //if (playerposition.y + MoveY <= -4.5 || playerposition.y + MoveY >= 3) MoveY = 0;
+        //transform.Translate(MoveX, MoveY, 0);
+        MoveX = Input.GetAxis("Horizontal");
+        float MoveY = Input.GetAxis("Vertical");
+        Vector2 movingVector = new Vector2(MoveX,MoveY);
+        rd.AddForce(movingVector * PlayerSpeed*Time.deltaTime);
+    }
+}
